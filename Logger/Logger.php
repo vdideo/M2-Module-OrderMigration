e2a08bd2c18eb1abc6e06199327edd58d393101b
Enrico Falappa <e.falappa@tun2u.com>
2022-09-06 19:29:46 +0200

    addRecord

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -22,6 +22,12 @@ class Logger extends \Monolog\Logger
 	protected $configData;
 
 	/**
+	 * @psalm-param array<callable(array): array> $processors
+	 *
+	 * @param string             $name       The logging channel, a simple descriptive name that is attached to all log records
+	 * @param HandlerInterface[] $handlers   Optional stack of handlers, the first one in the array is called first, etc.
+	 * @param callable[]         $processors Optional array of processors
+	 * @param DateTimeZone|null  $timezone   Optional timezone, if not provided date_default_timezone_get() will be used
 	 * @param ConfigData $configData
 	 */
 	public function __construct(string $name, array $handlers = [], array $processors = [], ?DateTimeZone $timezone = null, ConfigData $configData)
@@ -47,70 +53,6 @@ class Logger extends \Monolog\Logger
 			return null;
 		}
 
-		if (isset(self::RFC_5424_LEVELS[$level])) {
-			$level = self::RFC_5424_LEVELS[$level];
-		}
-
-		if ($this->detectCycles) {
-			$this->logDepth += 1;
-		}
-		if ($this->logDepth === 3) {
-			$this->warning('A possible infinite logging loop was detected and aborted. It appears some of your handler code is triggering logging, see the previous log record for a hint as to what may be the cause.');
-			return false;
-		} elseif ($this->logDepth >= 5) { // log depth 4 is let through so we can log the warning above
-			return false;
-		}
-
-		try {
-			$record = null;
-
-			foreach ($this->handlers as $handler) {
-				if (null === $record) {
-					// skip creating the record as long as no handler is going to handle it
-					if (!$handler->isHandling(['level' => $level])) {
-						continue;
-					}
-
-					$levelName = static::getLevelName($level);
-
-					$record = [
-						'message' => $message,
-						'context' => $context,
-						'level' => $level,
-						'level_name' => $levelName,
-						'channel' => $this->name,
-						'datetime' => $datetime ?? new DateTimeImmutable($this->microsecondTimestamps, $this->timezone),
-						'extra' => [],
-					];
-
-					try {
-						foreach ($this->processors as $processor) {
-							$record = $processor($record);
-						}
-					} catch (Throwable $e) {
-						$this->handleException($e, $record);
-
-						return true;
-					}
-				}
-
-				// once the record exists, send it to all handlers as long as the bubbling chain is not interrupted
-				try {
-					if (true === $handler->handle($record)) {
-						break;
-					}
-				} catch (Throwable $e) {
-					$this->handleException($e, $record);
-
-					return true;
-				}
-			}
-		} finally {
-			if ($this->detectCycles) {
-				$this->logDepth--;
-			}
-		}
-
-		return null !== $record;
+		return parent::addRecord($level, $message, $context, $datetime);
 	}
 }
